3c38eabd6b5f2bbe89611a0b126ece8d951fb558
BeginnerAbhi <dhooliabhinav@gmail.com>
2024-11-22 23:02:59 +0530

    Add initial JavaScript functions

---
+++ index.js
@@ -0,0 +1,22 @@
+// Function to find even numbers in an array
+function findEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
+
+// Function to find odd numbers in an array
+function findOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+
+// Function to print a pattern
+function printPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(i));
+    }
+}
+
+// Test the functions
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log("Even Numbers:", findEvenNumbers(numbers));
+console.log("Odd Numbers:", findOddNumbers(numbers));
+printPattern(5);
